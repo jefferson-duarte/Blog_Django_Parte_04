168396186328e36011441e3e0fc2d119f21cd6d6
Jefferson Duarte <jeffersonsduarte@live.com>
2022-05-10 11:17:30 +0100

    Projeto Blog Parte 04

---
+++ posts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+from .models import Post
+
+
+class PostIndex(ListView):
+    model = Post
+    template_name = 'posts/index.html'
+    paginate_by = 3
+    context_object_name = 'posts'
+
+
+class PostBusca(PostIndex):
+    pass
+
+
+class PostCategoria(PostIndex):
+    pass
+
+
+class PostDetalhes(UpdateView):
+    pass
